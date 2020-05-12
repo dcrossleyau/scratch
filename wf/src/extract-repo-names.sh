18a0b0a663af9a4ef3e688a3d96959d554eecd43
David Crossley <dcrossley@indexgeo.com.au>
2020-05-12 19:40:26 +1000

    Reduce to basic output

--- wf/src/extract-repo-names.sh
+++ wf/src/extract-repo-names.sh
@@ -1,11 +1,2 @@
-echo "Using bash ${BASH_VERSION}"
-
 INPUT_PN="${GITHUB_WORKSPACE}/wf/repos.json"
-
-echo "Processing JSON file ${INPUT_PN} ..."
-
-readarray -t repo_list < <(jq -r '.repos[].name' < ${INPUT_PN})
-count_total=$((${#repo_list[@]} -1))
-echo "count_total=${count_total}"
-echo ${repo_list[@]}
-
+jq -r '.repos[].name' < ${INPUT_PN}
